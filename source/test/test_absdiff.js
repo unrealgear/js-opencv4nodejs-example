79e2c6339030743c2a45033739ab06da51a4fb11
Nikita Riabchenko <unrealgear@gmail.com>
2019-01-30 17:22:22 +0200

    Fixed wrong file name in absdiff example.

--- source/test/test_absdiff.js
+++ source/test/test_absdiff.js
@@ -3,7 +3,7 @@ const cv = require("opencv4nodejs");
 describe('Checking absdiff matching', function () {
     it('Default example', async function () {
         const actual = await cv.imreadAsync('./images/windows_actual.png');
-        const expected = await cv.imreadAsync('./images/clean.png');
+        const expected = await cv.imreadAsync('./images/windows_expected.png');
 
         let matched = actual.absdiff(expected);
 
